5f97ed035d9706be15c261afee1c12ca52a82a2a
mbernallaura <mbernallaura@gmail.com>
2023-01-31 15:53:21 -0500

    Variables en HTML

--- src/firstApp.jsx
+++ src/firstApp.jsx
@@ -1,17 +1,30 @@
+const newMessage = [1,2,3,4,5,6,7,8,9];
+const objeto = {
+    message: 'Hola Mundo',
+    title: 'Laura',
+}
+
+const Saludo = () =>{
+    return `Hola ${objeto.title}`;
+}
 
 export const FirstApp = () =>{
     //!Simpre hay que regresar al menos un Nodo Padre y adentro puede llevar varios elementos
-    //!El simbolo para "Fragmentar el codigo es <> </>"
+    //!Si no se quiere regresar un div para enviar varios elementos se puede usar <> </>
+    //!Para fragentar el codigo"
+
+    //!Se puede devolver Funciones que NO SEAN ASYNCRONAS
     return (
         <> 
-            <h1>Laura!!</h1>
-            <p>Parrafo</p>
-            <>
+            <h1>{ newMessage }</h1>
+            <p>{ Saludo() }</p>
+            {/* <code>{ JSON.stringify( objeto ) }</code> */}
+            <div>
                 <ul>
                     <li>Nombre</li>
                     <li>Apellido</li>
                 </ul>
-            </>
+            </div>
         </>
     )
 }
