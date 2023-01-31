353a9e5c57ea8b261d5be900a2e97b719852587c
mbernallaura <mbernallaura@gmail.com>
2023-01-31 16:00:03 -0500

    Ponerle estilos globales al proyecto

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HiWorldApp } from './HiWorldApp';
+//import { HiWorldApp } from './HiWorldApp';
 import { FirstApp } from './firstApp';
+import './styles.css';
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
