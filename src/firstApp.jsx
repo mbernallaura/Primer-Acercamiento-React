52d819d603ac19f7b2c0607965f9174073d45a76
mbernallaura <mbernallaura@gmail.com>
2023-01-31 15:29:16 -0500

    Poder regresar varios elementos dentro de un retorno

--- src/firstApp.jsx
+++ src/firstApp.jsx
@@ -1,3 +1,17 @@
+
 export const FirstApp = () =>{
-    return <h1>Laura!!</h1>
+    //!Simpre hay que regresar al menos un Nodo Padre y adentro puede llevar varios elementos
+    //!El simbolo para "Fragmentar el codigo es <> </>"
+    return (
+        <> 
+            <h1>Laura!!</h1>
+            <p>Parrafo</p>
+            <>
+                <ul>
+                    <li>Nombre</li>
+                    <li>Apellido</li>
+                </ul>
+            </>
+        </>
+    )
 }
