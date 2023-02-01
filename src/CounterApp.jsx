ef9163585876ef9577025c49ca2bdacefec31a97
mbernallaura <mbernallaura@gmail.com>
2023-01-31 19:12:46 -0500

    Ejercicio utlizando hook, y porque cuado el estado cambia, se vuelve a cargar la funcion

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,21 +4,28 @@ import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) =>{
     
+    //!Cuando el estado (useState) cambia, vulve y carga la funcion CounterApp
     const [counter, setCounter] = useState( value );
 
-    const handleAdd = () => {
+    const incremento = () => {
         //!De ambas maneras se puede hacer
         //setCounter((c) => c + 1 );
         setCounter( counter + 1);
     }
 
+    const decremento = () => setCounter(counter - 1);
+    
+
+    const reset = () => setCounter(value);
+    
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{ counter }</h2>
-            <button onClick={ handleAdd }>
-                +1
-            </button>
+            <button onClick={ incremento }> +1 </button>
+            <button onClick={ decremento }> -1 </button>
+            <button onClick={ reset }> Reset </button>
         </>
     )
 }
