f4ef8436acca93ddd07c3ad54ff3951049d8dec5
mbernallaura <mbernallaura@gmail.com>
2023-02-06 18:43:49 -0500

    Pruebas con container, getByText

--- tests/firstApp.test.jsx
+++ tests/firstApp.test.jsx
@@ -2,8 +2,25 @@ import { render } from "@testing-library/react";
 import { FirstApp } from "../src/firstApp";
 
 describe('Pruebas en <firstApp/>', () => {
+
     test('Debe de compaginar con mi componente', () => {
         const title = 'Hola soy Goku';
-        render(<FirstApp title={ title }/>)
+        const {container} = render(<FirstApp title={ title }/>)
+
+        //! toMatchSnapshot() = Hace una captura del componente y lo guarda enn un archivo que lo crea
+        expect(container).toMatchSnapshot();
     });
+
+    test('Debe mostar el titulo en un H1', () => {
+        const title = 'Hola soy Goku';
+        //! container = maneja lo del dom, por ende se puede manejar con JavaScript
+        const { container,getByText } = render(<FirstApp title={ title }/>)
+
+        //! getByText(title) = Busca en el render un texto que contenga "Hola soy Goku"
+        //! toBeTruthy() = que se encuentre lo que se coloca anteriormente
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title);
+    })
 });
